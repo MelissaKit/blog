5b13aef10bf683ff75c9bb6370b06280beb71aeb
MelissaKit <voz.15.1998@gmail.com>
2021-12-04 19:43:14 +0200

    try push

--- config/database.inc.php
+++ config/database.inc.php
@@ -1,9 +1,15 @@
 <?php
-    define('DATABASE_HOST', 'localhost');
-    define('DATABASE_USER', 'root');
-    define('DATABASE_PASS','');
+    $url = getenv("CLEARDB_DATABASE_URL");
+    if (empty($url)) {
+      define('DATABASE_HOST', 'localhost');
+      define('DATABASE_USER', 'root');
+      define('DATABASE_PASS','');
+    } else {
+      define('DATABASE_HOST', $url["host"]);
+      define('DATABASE_USER', $url["user"]);
+      define('DATABASE_PASS', $url["pass"]);
+    }
     define('DATABASE_DBNAME','CourseWork');
     define('MAILING_EMAIL', 'bestfilmever3@gmail.com');
     define('MAILING_PASSWORD', 'bestfilmever123');
-
 ?>
